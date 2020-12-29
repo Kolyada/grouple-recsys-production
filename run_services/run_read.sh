b8850dc20b7190f1afe296428698458f8d401562
ARQtty <ShamovIA@yandex.ru>
2020-12-29 19:25:10 +0300

    Fix current dir running support

--- run_services/run_read.sh
+++ run_services/run_read.sh
@@ -1,5 +1,6 @@
-docker run --volume /home/iria/grouple/production/recommendations_server:/recommendations_server \
+docker run --volume $(pwd)/../recommendations_server:/recommendations_server \
            --volume /data/groupLe_recsys/processed:/data/groupLe_recsys/processed \
            -p 5002:5000 \
            -e CONFIG_FILE=manga_setting.yml \
+           -d \
        sagiri
--- run_services/run_selfmanga.sh
+++ run_services/run_selfmanga.sh
@@ -1,5 +1,6 @@
-docker run --volume /home/iria/grouple/production/recommendations_server:/recommendations_server \
+docker run --volume $(pwd)/../recommendations_server:/recommendations_server \
            --volume /data/groupLe_recsys/processed:/data/groupLe_recsys/processed \
            -p 5013:5000 \
            -e CONFIG_FILE=selfmanga_setting.yml \
+           -d \
        sagiri
